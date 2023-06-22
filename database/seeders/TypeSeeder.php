aa8669b035ae9e6eef02280fe1d3b76ae4d319fa
Lucianor01 <lucianorosario02@gmail.com>
2023-06-22 22:39:10 +0200

    type migration seeder e model

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +16,19 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = [
+            'FrontEnd',
+            'Backend',
+            'FullStack',
+            'Design'
+        ];
+
+
+        foreach ($types as $elem) {
+            $new_type = new Type();
+            $new_type->name = $elem;
+            $new_type->slug = Str::slug($new_type->name);
+            $new_type->save();
+        }
     }
 }
